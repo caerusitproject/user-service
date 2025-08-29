875739beb4c0154dcf2fa4180413b858122b27cf
Komal Singh <komalcaerusit@gmail.com>
2025-08-29 13:24:45 +0530

    Refactored user service

---
+++ src/main/java/com/caerus/userservice/dto/UserUpdateDto.java
@@ -0,0 +1,38 @@
+package com.caerus.userservice.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserUpdateDto {
+
+    private Long id;
+
+    @NotBlank(message = "First name should not be empty")
+    private String firstName;
+
+    @NotEmpty(message = "Last name should not be empty")
+    private String lastName;
+
+    @Email
+    private String email;
+
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "Password must be at least 8 characters, contain at least one uppercase letter and one number"
+    )
+    private String password;
+
+    @Pattern(regexp = "\\d{10}", message = "Phone number should have exactly 10 digits")
+    private String phone;
+
+    private Boolean isActive;
+
+    private Set<String> role;
+}
--- src/main/java/com/caerus/userservice/service/UserService.java
+++ src/main/java/com/caerus/userservice/service/UserService.java
@@ -1,6 +1,7 @@
 package com.caerus.userservice.service;
 
 import com.caerus.userservice.dto.UserDto;
+import com.caerus.userservice.dto.UserUpdateDto;
 import com.caerus.userservice.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +20,6 @@ public interface UserService {
 
 	UserDto findUserByUsername(String username);
 
-    UserDto updateUserById(Long userId, UserDto userDto);
+    UserDto updateUserById(Long userId, UserUpdateDto userUpdateDto);
 
 }
